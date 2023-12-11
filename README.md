dfb0c337c8b362b9fb19c81c8e0ff837baa86260
FloWa <fwalzel@gmx.de>
2023-12-11 22:06:43 +0100

    feat: added files from private repo

--- README.md
+++ README.md
@@ -1,2 +1,204 @@
-# async-file-tried
-A try-catch wrapper around node’s fs/promises.
+# Async-file-tried
+
+*async-file-tried* is a wrapper around node’s `fs/promises` that abstracts the try-catch block for you.
+Write more linear, more concise code by getting a smoothed response. TypeScript supported.
+
+## License
+
+Copyright (c) 2023 Florian Walzel,
+MIT License
+
+## Install
+
+```sh
+npm install async-file-tried
+```
+
+Import:
+
+```javascript
+import * as fs from 'async-file-tried';
+```
+
+## Usage
+
+Usually we do not want to make calls against the file system without a proper error handling. But the try-catch block is somewhat unelegant because it moves code blocks in the bracket sub-space and thus disturbs the linearity of our programming. *async-file-tried* returns a tuple with either the response or the error from a fs call and simplifies error handling.
+
+You can write now:
+
+```javascript
+let [res, err] = await fs.readdir('.');
+if (err) console.error(err);
+else console.log(res);
+```
+
+... instead of:
+
+```javascript
+try {
+    let res = await fs.readdir('.');
+    console.log(res);
+}
+catch (err) {
+    console.error(err);
+}
+```
+
+## Extra: joined paths
+
+*async-file-tried* also has an in-built path.join() so that you can alternatively pass all path arguments as array of sub-elements.
+
+You can write i.e.:
+
+```javascript
+let [res, err] = await fs.appendFile(['fs.__dirname', '..' , 'myfolder', `img_${i}.txt`], 'my text');
+```
+
+... and the file string will be composed automatically.
+
+## API
+
+### FS Function List
+
+- [fs.access](https://nodejs.org/api/fs.html#fspromisesaccesspath-mode)
+  + Typescript implementation: `async (path: string|Array<string>, mode?: number|string)`
+  + Usage example: `let [res, err] = await fs.access('file.txt', fs.constants.R_OK);`
+
+- [fs.appendFile](https://nodejs.org/api/fs.html#fspromisesappendfilepath-data-options)
+  + Typescript implementation: `async (path: string|Array<string>, data: any, options?: { encoding?: Encoding; mode?: number|string; flag?: Flags; })`
+  + Usage example: `let [res, err] = await fs.appendFile('file.txt', 'foo');`
+
+- [fs.chmod](https://nodejs.org/api/fs.html#filehandlechmodmode)
+  + Typescript implementation: `async (path: string|Array<string>, mode?: number|string)`
+  + Usage example: `let [res, err] = await fs.chmod('file.txt', '755');`
+
+- [fs.chown](https://nodejs.org/api/fs.html#filehandlechownuid-gid)
+  + Typescript implementation: `async (path: string|Array<string>, uid: number, gid: number)`
+  + Usage example: `let [res, err] = await fs.chown('file.txt', 1541, 999);`
+
+- [fs.copyFile](https://nodejs.org/api/fs.html#fspromisescopyfilesrc-dest-mode)
+  + Typescript implementation: `async (srcPath: string|Array<string>, destPath: string|Array<string>,flags?: number)`
+  + Usage example: `let [res, err] = await fs.copyFile('file.txt', 'file-copy.txt');`
+
+- [fs.cp](https://nodejs.org/api/fs.html#fspromisescpsrc-dest-options)
+  + Typescript implementation: `async (srcPath: string|Array<string>, destPath: string|Array<string>)`
+  + Usage example: `let [res, err] = await fs.cp('myfolder', 'myfolder-copy', {recursive: true});`
+
+- [fs.lchmod](https://nodejs.org/api/fs.html#fspromiseslchmodpath-mode)
+  + Typescript implementation: `async (path: string|Array<string>, mode?: number|string)`
+  + Usage example: ``
+
+- [fs.lchown](https://nodejs.org/api/fs.html#fspromiseslchownpath-uid-gid)
+  + Typescript implementation: `async (path: string|Array<string>, uid: number, gid: number)`
+  + Usage example: `let [res, err] = await fs.lchown('./test/static-testfiles/symlinkToFile-append.txt', 1200, 1201);`
+
+- [fs.link](https://nodejs.org/api/fs.html#fspromiseslinkexistingpath-newpath)
+  + Typescript implementation: `async (existingPath: string|Array<string>, newPath: string|Array<string>)`
+  + Usage example: `let [res, err] = await fs.link('file.txt', 'file-hard-link.txt');`
+
+- [fs.lstat](https://nodejs.org/api/fs.html#fspromiseslstatpath-options)
+   + Typescript implementation: `async (path: string|Array<string>, options?: object)`
+   + Usage example: `let [res, err] = await fs.lstat('symlinkToFile.txt');`
+
+- [fs.lutimes](https://nodejs.org/api/fs.html#fspromiseslutimespath-atime-mtime)
+  + Typescript implementation: `async (path: string|Array<string>, atime: Date|number, mtime: Date|number)`
+  + Usage example: `let [res, err] = await fs.lutimes('file.txt', new Date(), new Date());`
+
+- [fs.mkdir](https://nodejs.org/api/fs.html#fspromisesmkdirpath-options)
+  + Typescript implementation: `async (path: string|Array<string>, options?: number|string)`
+  + Usage example: `let [res, err] = await fs.mkdir('./my-new-folder');`
+
+- [fs.mkdtemp](https://nodejs.org/api/fs.html#fspromisesmkdtempprefix-options)
+  + Typescript implementation: `async (prefix: string, encoding?: Encoding)`
+  + Usage example: `let [res, err] = await fs.mkdtemp('temp-');`
+
+- [fs.open](https://nodejs.org/api/fs.html#fspromisesopenpath-flags-mode)
+  + Typescript implementation: `async (path: string|Array<string>, flags?: Flags, mode?: number|string)`
+  + Usage example: `let [res, err] = await fs.open('file.txt', 'r');`
+
+- [fs.opendir](https://nodejs.org/api/fs.html#fspromisesopendirpath-options)
+  + Typescript implementation: `async (path: string|Array<string>, flags?: Flags, mode?: number|string)`
+  + Usage example: `let [res, err] = await fs.opendir('./test', { encoding: "utf8", bufferSize: 64 } );`
+
+- [fs.readFile](https://nodejs.org/api/fs.html#filehandlereadfileoptions)
+  + Typescript implementation: `async (path: string|Array<string>, options?: Encoding)`
+  + Usage example: `let [res, err] = await fs.readFile('file.txt', 'utf8');`
+
+- [fs.readdir](https://nodejs.org/api/fs.html#filehandlereadfileoptions)
+  + Typescript implementation: `async (path: string|Array<string>, options?: object)`
+  + Usage example: `let [res, err] = await fs.readdir('./my-directory');`
+
+- [fs.readlink](https://nodejs.org/api/fs.html#fspromisesreadlinkpath-options)
+  + Typescript implementation: `async (path: string|Array<string>, options?: object|string)`
+  + Usage example: `let [res, err] = await fs.readlink('symlinkToFile.txt');`
+
+- [fs.realpath](https://nodejs.org/api/fs.html#fspromisesrealpathpath-options)
+  + Typescript implementation: `async (path: string|Array<string>, options?: object)`
+  + Usage example: `let [res, err] = await fs.realpath('./my-folder/../' );`
+
+- [fs.rename](https://nodejs.org/api/fs.html#fspromisesrenameoldpath-newpath)
+  + Typescript implementation: `async (oldPath: string|Array<string>, newPath: string|Array<string>)`
+  + Usage example: `let [res, err] = await fs.rename('old.txt', 'new.txt' );`
+
+- [fs.rm](https://nodejs.org/api/fs.html#fspromisesrenameoldpath-newpath)
+  + Typescript implementation: `async (path: string|Array<string>, options?: object)`
+  + Usage example: `let [res, err] = await fs.rm('file.txt' );`
+
+- [fs.rmdir](https://nodejs.org/api/fs.html#fspromisesrmdirpath-options)
+  + Typescript implementation: `async (path: string|Array<string>, options?: object)`
+  + Usage example: `let [res, err] = await fs.rmdir('./my-folder');`
+
+- [fs.stat](https://nodejs.org/api/fs.html#fspromisesstatpath-options)
+  + Typescript implementation: `async (path: string|Array<string>, options?: object)`
+  + Usage example: `let [res, err] = await fs.stat('file.txt');`
+
+-[fs.symlink](https://nodejs.org/api/fs.html#fspromisessymlinktarget-path-type)
+  + Typescript implementation: `async (target: string|Array<string>, path: string|Array<string>, type?: string)`
+  + Usage example: `let [res, err] = await fs.symlink('file.txt', 'file-symlink.txt', 'file');`
+
+-[fs.truncate](https://nodejs.org/api/fs.html#fspromisestruncatepath-len)
+  + Typescript implementation: `async (path: string|Array<string>, len:number)`
+  + Usage example: `let [res, err] = await fs.truncate('file.txt', 760);`
+
+-[fs.unlink](https://nodejs.org/api/fs.html#fspromisesunlinkpath)
+  + Typescript implementation: `async (path: string|Array<string>)`
+  + Usage example: `let [res, err] = await fs.unlink('file-symlink.txt');`
+
+-[fs.utimes](https://nodejs.org/api/fs.html#fspromisesutimespath-atime-mtime)
+  + Typescript implementation: `async (path: string|Array<string>, atime: number|string|Date, mtime: number|string|Date)`
+  + Usage example: `let [res, err] = await fs.utimes('file.txt', new Date(), new Date());`
+
+-[fs.watch](https://nodejs.org/api/fs.html#fspromiseswatchfilename-options)
+  + Typescript implementation: `async (filename: string|Array<string>, options?: object|string)`
+  + Usage example: `let [res, err] = await fs.watch('file.txt', (ev, file) => { console.log("Watcher: " + file); });`
+
+-[fs.writeFile](https://nodejs.org/api/fs.html#fspromiseswritefilefile-data-options)
+  + Typescript implementation: `async (path: string|Array<string>, data: any, options?: Encoding)`
+  + Usage example: `let [res, err] = await fs.writeFile('file.txt', 'my text', 'utf8');`
+
+
+### The Constants
+
+-[fs.constants](https://nodejs.org/api/fs.html#fspromisesconstants)
+  + The FS [constants](https://nodejs.org/api/fs.html#fs-constants) object
+
+-[fs.__dirname](https://nodejs.org/docs/latest/api/globals.html#__dirname)
+  + The equivalent to node’s `__dirname` usable within ES6 Module Syntax. Returns the directory name of the current module.
+  + Implementation: `path.dirname(fileURLToPath(import.meta.url));`
+
+-[fs.__filename](https://nodejs.org/docs/latest/api/globals.html#__filename)
+  + The equivalent to node’s `__filename` usable within ES6 Module Syntax. Returns the filename of the code which is executed.
+  + Implementation: `fileURLToPath(import.meta.url);`
+
+
+### Bonus Functions
+
+- fs.readJson
+  + Reads a JSON file and returns it as parsed Javascript object.
+  + Typescript implementation: `async (path: string|Array<string>, options?: Encoding)`
+  + Usage example: `let [res, err] = await fs.readJson('my.json');`
+
+- fs.writeJson
+  + Expects a Javascript object and will stringify and write it out.
+  + Typescript implementation: `async (path: string|Array<string>, data: Object, options?: Encoding)`
+  + Usage example: `let [res, err] = await fs.writeJson('./test/static-testfiles/test.json', { key : "value" });`
---
+++ test/index.test.js
@@ -0,0 +1,393 @@
+/**
+ * Tests for ./dist/index.js
+ *
+ * usage:
+ * $ npm test
+ */
+
+import * as chai from 'chai';
+import fs from '../dist/index.js';
+
+const assert = chai.assert;
+const expect = chai.expect;
+
+describe('Tests for async-file-tried', () => {
+
+  /****************************************
+   * TEST INTERFACE
+   ****************************************/
+
+  /**
+   * THE CONSTANTS
+   */
+
+  it('[1] OBJ: fs.constants shall be an object', () => {
+    assert.isObject(fs.constants);
+  });
+
+  it('[2] STR: fs.__dirname shall be a string starting with "/"', () => {
+    expect(fs.__dirname.startsWith("/")).to.be.true;
+  });
+
+  it('[3] STR: fs.__filename shall be a string starting with "/" and ends with ".js"', () => {
+    expect(fs.__filename.startsWith("/")).to.be.true;
+    expect(fs.__filename.endsWith(".js")).to.be.true;
+  });
+
+  /**
+   * THE FS/PROMISE FUNCTIONS
+   */
+
+  it('[4] ERR: fs.access shall error when file inaccessible', async () => {
+    let [res, err] = await fs.access('', fs.constants.R_OK);
+    assert.notEqual(err, null);
+  });
+
+  it('[5] RES: fs.access shall return valid result when file is accessible', async () => {
+    let [res, err] = await fs.access('./test/static-testfiles/test.json', fs.constants.R_OK);
+    assert.equal(err, null);
+  });
+
+  it('[6] ERR: fs.appendFile shall error when file does not exist', async () => {
+    let [res, err] = await fs.appendFile('', 'some text');
+    assert.notEqual(err, null);
+  });
+
+  it('[7] RES: fs.appendFile shall return valid result when file was appended', async () => {
+    let [res, err] = await fs.appendFile('./test/static-testfiles/append.txt', 'some text');
+    assert.equal(err, null);
+  });
+
+  it('[8] ERR: fs.chmod shall error when file does not exist', async () => {
+    let [res, err] = await fs.chmod('', '755');
+    assert.notEqual(err, null);
+  });
+
+  it('[9] RES: fs.chmod shall return valid result when file rights where changed', async () => {
+    let [res, err] = await fs.chmod('./test/static-testfiles/append.txt', '755');
+    assert.equal(err, null);
+  })
+
+  it('[10] ERR: fs.chown shall error when file does not exist', async () => {
+    let [res, err] = await fs.chown('', '755');
+    assert.notEqual(err, null);
+  });
+
+  /*it('[10A] RES: fs.chown shall return valid result when file rights where changed', async () => {
+    let [res, err] = await fs.chown('./test/static-testfiles/append.txt', 1541, 999);
+    console.log(err);
+    assert.equal(err, null);
+  });*/
+
+  it('[11] ERR: fs.copyFile shall error when file does not exist', async () => {
+    let [res, err] = await fs.copyFile('', '');
+    assert.notEqual(err, null);
+  });
+
+  it('[12] RES: fs.copyFile shall return valid result when file rights was copied', async () => {
+    let [res, err] = await fs.copyFile('./test/static-testfiles/append.txt', './test/static-testfiles/append-copy.txt');
+    assert.equal(err, null);
+  })
+
+  it('[13] ERR: fs.link shall error when file does not exist', async () => {
+    let [res, err] = await fs.link('', '');
+    assert.notEqual(err, null);
+  });
+
+  it('[14] RES: fs.link shall return valid result when link was set', async () => {
+    let [res, err] = await fs.link('./test/static-testfiles/append.txt', './test/static-testfiles/hardlinkToFile-append.txt');
+    assert.equal(err, null);
+  });
+
+  it('[15] ERR: fs.unlink shall error when file does not exist', async () => {
+    let [res, err] = await fs.unlink('');
+    assert.notEqual(err, null);
+  });
+
+  it('[16] RES: fs.unlink shall error return valid result when files was removed', async () => {
+    let [res, err] = await fs.unlink('./test/static-testfiles/hardlinkToFile-append.txt');
+    assert.equal(err, null);
+  });
+
+  it('[17] ERR: fs.symlink shall error when file does not exist', async () => {
+    let [res, err] = await fs.symlink('', '', 'file');
+    assert.notEqual(err, null);
+  });
+
+  it('[18] RES: fs.symlink shall return valid result when symlink was set', async () => {
+    let [res, err] = await fs.symlink('./test/static-testfiles/append.txt', './test/static-testfiles/symlinkToFile-append.txt', 'file');
+    assert.equal(err, null);
+  });
+
+  it('[19] ERR: fs.lchmod shall error when file does not exist', async () => {
+    let [res, err] = await fs.lchmod('', '755');
+    assert.notEqual(err, null);
+  });
+
+  it('[20] RES: fs.lchmod shall return valid result when file rights where changed', async () => {
+    let [res, err] = await fs.lchmod('./test/static-testfiles/symlinkToFile-append.txt', '755');
+    assert.equal(err, null);
+  })
+
+  it('[21] ERR: fs.lchown shall error when file does not exist', async () => {
+    let [res, err] = await fs.lchown('', 1541, 1541);
+    assert.notEqual(err, null);
+  });
+
+  /*
+  it('[21A] RES: fs.lchown shall return valid result when file rights where changed', async () => {
+   let [res, err] = await fs.lchown('./test/static-testfiles/symlinkToFile-append.txt', 1200, 1201);
+   console.log(err);
+   assert.equal(err, null);
+ });*/
+
+  it('[22] ERR: fs.lstat shall error when file does not exist', async () => {
+    let [res, err] = await fs.lstat('');
+    assert.notEqual(err, null);
+  });
+
+  it('[23] RES: fs.lstat shall return valid result when symlink stats where retrieved', async () => {
+    let [res, err] = await fs.lstat('./test/static-testfiles/symlinkToFile-append.txt');
+    assert.equal(err, null);
+  });
+
+  it('[24] ERR: fs.readlink shall error when link does not exist', async () => {
+    let [res, err] = await fs.readlink('');
+    assert.notEqual(err, null);
+  });
+
+  it('[25] RES: fs.lstat shall return valid result when symlink origin was read', async () => {
+    let [res, err] = await fs.readlink('./test/static-testfiles/symlinkToFile-append.txt');
+    assert.equal(err, null);
+  });
+
+  it('[26] ERR: fs.lutimes shall error when file does not exist', async () => {
+    let [res, err] = await fs.lutimes('', '', '');
+    assert.notEqual(err, null);
+  });
+
+  it('[27] RES: fs.lutimes shall return valid result when modification date and access time where changed', async () => {
+    let [res, err] = await fs.lutimes('./test/static-testfiles/append.txt', new Date(), new Date());
+    assert.equal(err, null);
+  });
+
+  it('[28] ERR: fs.mkdir shall error when dirname is empty', async () => {
+    let [res, err] = await fs.mkdir('');
+    assert.notEqual(err, null);
+  });
+
+  it('[29] RES: fs.mkdir shall return valid result when new folder was created', async () => {
+    let [res, err] = await fs.mkdir('./test/static-testfiles/my-new-folder');
+    assert.equal(err, null);
+  });
+
+  it('[30] ERR: fs.cp shall error when file does not exist', async () => {
+    let [res, err] = await fs.cp('', '');
+    assert.notEqual(err, null);
+  });
+
+  it('[31] RES: fs.cp shall return valid result when file rights was copied', async () => {
+    let [res, err] = await fs.cp('./test/static-testfiles/my-new-folder/', './test/static-testfiles/my-new-folder-copy/', {recursive: true});
+    assert.equal(err, null);
+  })
+
+  it('[32] ERR: fs.mkdtemp shall error when dirname is invalid', async () => {
+    let [res, err] = await fs.mkdtemp({ });
+    assert.notEqual(err, null);
+  });
+
+  it('[33] RES: fs.mkdtemp shall return valid result when new temporary folder was created', async () => {
+    let [res, err] = await fs.mkdtemp('./test/static-testfiles/temp-');
+    assert.equal(err, null);
+    // cleanup
+    await fs.rmdir(res);
+  });
+
+  it('[34] ERR: fs.open shall error when file does not exist', async () => {
+    let [res, err] = await fs.open('');
+    assert.notEqual(err, null);
+  });
+
+  it('[35] RES: fs.open shall return valid result when file was opened', async () => {
+    let [res, err] = await fs.open('./test/static-testfiles/append.txt', 'r');
+    assert.equal(err, null);
+  });
+
+  it('[36] ERR: fs.opendir shall error when file does not exist', async () => {
+    let [res, err] = await fs.opendir('');
+    assert.notEqual(err, null);
+  });
+
+  it('[37] RES: fs.opendir shall return valid result when file was opened', async () => {
+    let [res, err] = await fs.opendir('./test/static-testfiles', { encoding: "utf8", bufferSize: 64 } );
+    assert.equal(err, null);
+  });
+
+  it('[38] ERR: fs.readFile shall error when file does not exist', async () => {
+    let [res, err] = await fs.readFile('');
+    assert.notEqual(err, null);
+  });
+
+  it('[39] RES: fs.readFile shall return valid result when file was read', async () => {
+    let [res, err] = await fs.readFile('./test/static-testfiles/append.txt' );
+    assert.equal(err, null);
+  });
+
+  it('[40] ERR: fs.readdir shall error when directory does not exist', async () => {
+    let [res, err] = await fs.readdir('');
+    assert.notEqual(err, null);
+  });
+
+  it('[41] RES: fs.readdir shall return valid result when directory was read', async () => {
+    let [res, err] = await fs.readdir('./test/static-testfiles/' );
+    assert.equal(err, null);
+  });
+
+  it('[42] ERR: fs.realpath shall error when directory does not exist', async () => {
+    let [res, err] = await fs.realpath('');
+    assert.notEqual(err, null);
+  });
+
+  it('[43] RES: fs.realpath shall return valid result when real path was resolved', async () => {
+    let [res, err] = await fs.realpath('./test/../' );
+    assert.equal(err, null);
+  });
+
+  it('[44] ERR: fs.rename shall error when directory does not exist', async () => {
+    let [res, err] = await fs.rename('', '');
+    assert.notEqual(err, null);
+  });
+
+  it('[45] RES: fs.rename shall return valid result when file was renamed', async () => {
+    let [res, err] = await fs.rename('./test/static-testfiles/append.txt', './test/static-testfiles/append-renamed.txt' );
+    assert.equal(err, null);
+  });
+
+  it('[46] ERR: fs.rm  shall error when directory or file does not exist', async () => {
+    let [res, err] = await fs.rm('');
+    assert.notEqual(err, null);
+  });
+
+  it('[47] RES: fs.rm shall return valid result when file was removed', async () => {
+    let [res, err] = await fs.rm('./test/static-testfiles/append-renamed.txt' );
+    assert.equal(err, null);
+  });
+
+  it('[48] ERR: fs.rmdir shall error when directory or file does not exist', async () => {
+    let [res, err] = await fs.rmdir('');
+    assert.notEqual(err, null);
+  });
+
+  it('[49] RES: fs.rmdir shall return valid result when directory was removed', async () => {
+    let [res, err] = await fs.rmdir('./test/static-testfiles/my-new-folder');
+    assert.equal(err, null);
+  });
+
+  it('[50] ERR: fs.stat shall error when directory or file does not exist', async () => {
+    let [res, err] = await fs.stat('');
+    assert.notEqual(err, null);
+  });
+
+  it('[51] RES: fs.stat shall return valid result when file can be accessed for stats', async () => {
+    let [res, err] = await fs.stat('./test/static-testfiles/test.json');
+    assert.equal(err, null);
+  });
+
+  it('[52] ERR: fs.writeFile shall error when filename invalid', async () => {
+    let [res, err] = await fs.writeFile('', '');
+    assert.notEqual(err, null);
+  });
+
+  it('[53] RES: fs.writeFile shall return valid result when file was written', async () => {
+    let [res, err] = await fs.writeFile('./test/static-testfiles/file.txt', 'my text');
+    assert.equal(err, null);
+  });
+
+  it('[54] ERR: fs.truncate shall error when file does not exist', async () => {
+    let [res, err] = await fs.truncate('');
+    assert.notEqual(err, null);
+  });
+
+  it('[55] RES: fs.truncate shall return valid result when file was shortened', async () => {
+    let [res, err] = await fs.truncate('./test/static-testfiles/file.txt', 0);
+    assert.equal(err, null);
+  });
+
+  it('[56] ERR: fs.utimes shall error when file does not exist', async () => {
+    let [res, err] = await fs.utimes('', '', '');
+    assert.notEqual(err, null);
+  });
+
+  it('[57] RES: fs.utimes shall return valid result when modification date and access time where changed', async () => {
+    let [res, err] = await fs.utimes('./test/static-testfiles/file.txt', new Date(), new Date());
+    assert.equal(err, null);
+  });
+
+  it('[58] RES: fs.watch shall return valid result when modification date and access time where changed', async () => {
+    let [res, err] = await fs.watch('./test/static-testfiles/file.txt', (ev, file) => {
+      console.log("Watcher: " + file);
+    });
+    assert.equal(err, null);
+  });
+
+  /**
+   * HELPER
+   */
+
+  it('[59] RES: fs.__resolvePath shall return joined string', async () => {
+    let res = fs.__resolvePath(['./', 'folder', 'file.txt']);
+    assert.isString(res);
+  });
+
+
+  /**
+   * EXTRAS
+   */
+
+  it('[60] ERR: fs.asyncHandler shall error when Promise fails', async () => {
+   async function stall() {
+     await new Promise((resolve, reject) => {
+       let x = false;
+       if (x) resolve("OK");
+       else reject("Error");
+     });
+   }
+   let [res, err] = await fs.asyncHandler( () => stall());
+   assert.notEqual(err, null);
+  });
+
+  it('[61] RES: fs.asyncHandler shall return valid result when Promise resolves', async () => {
+    async function stall() {
+      await new Promise(resolve => setTimeout(resolve, 2));
+    }
+    let [res, err] = await fs.asyncHandler( () => stall());
+    assert.equal(err, null);
+  });
+
+  it('[62] ERR: fs.writeJson shall error when file does not exist', async () => {
+    let [res, err] = await fs.writeJson('', '');
+    assert.notEqual(err, null);
+  });
+
+  it('[63] RES: fs.writeJson shall return valid result when JSON was written', async () => {
+    let [res, err] = await fs.writeJson('./test/static-testfiles/test.json', { key : "value" });
+    assert.equal(err, null);
+  });
+
+  it('[64] ERR: fs.readJson shall error when file does not exist', async () => {
+    let [res, err] = await fs.readJson('');
+    assert.notEqual(err, null);
+  });
+
+  it('[65] RES: fs.readJson shall return object when JSON was read', async () => {
+    let [res, err] = await fs.readJson('./test/static-testfiles/test.json', 'utf8');
+    assert.isObject(res);
+  });
+
+});
+
+// cleanup symlink from test run:
+await fs.unlink('./test/static-testfiles/symlinkToFile-append.txt');
+
+// cleanup symlink from test run:
+await fs.rm('./test/static-testfiles/file.txt');
